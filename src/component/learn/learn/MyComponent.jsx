48dde03738a6e55e7a577141bf1f2b57a9313175
Trinh <23730135@ms.uit.com>
2025-02-14 08:51:04 +0700

    Video 30

--- src/component/learn/learn/MyComponent.jsx
+++ src/component/learn/learn/MyComponent.jsx
@@ -1,7 +1,11 @@
 // component = html + css +
+import './style.css';
 const MyComponent = () => {
-  return (
-    <div> Thuc hanh component - tris </div>
+    return (
+      <>
+            <div> Thuc hanh component - tris </div>
+            <div className='child'>child</div>
+      </>
   )
 }
 

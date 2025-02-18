a1620dd3a59fdc16576f0c28587b228be88e3b90
Trinh <23730135@ms.uit.com>
2025-02-18 22:36:29 +0700

    Video 33 component todo app

---
+++ src/component/learn/todo/Input.jsx
@@ -0,0 +1,12 @@
+let Input = () => {
+    return (
+        <>
+            <div className='input-text'>
+                <input type="text" placeholder='Enter your text' />
+                <button>Add</button>
+            </div>
+        </>
+    )
+}
+
+export default Input;
---
+++ src/component/learn/todo/TodoData.jsx
@@ -0,0 +1,13 @@
+
+let TodoData = () => {
+     return (
+
+          <>
+               <div className="todo-data">
+                    <div> Learning react</div>
+                    <div> Learning Java</div>
+               </div>
+          </>
+     )
+}
+export default TodoData;

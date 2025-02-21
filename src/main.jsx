bdd4f068b4899ead4043b46c8ee9567b61186670
Trinh <23730135@ms.uit.com>
2025-02-21 14:09:27 +0700

    change css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/login",
+    element: <div>login page</div>
+  }, ,
+  {
+    path: "/regrister",
+    element: <div>regrister page</div>
+  }, ,
+  {
+    path: "/login",
+    element: <div>login page</div>
+  }, ,
+  {
+    path: "/login",
+    element: <div>login page</div>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

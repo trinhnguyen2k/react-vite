6ea6ed7f1cb39cda70c9d89142ffc14ec173c734
Trinh <23730135@ms.uit.com>
2025-03-23 21:04:09 +0700

    video 67

--- src/component/user/user.form.jsx
+++ src/component/user/user.form.jsx
@@ -1,20 +1,43 @@
 import '../../styles/user.css';
 import { Button, Input } from 'antd';
+import { useState } from 'react';
 const UserForm = () => {
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [passWord, setpassWord] = useState('');
+    const [phoneNumber, setphoneNumber] = useState('');
+
+    const handleClickBtn = () => {
+        console.log('Check value: ', { fullName, email, passWord, phoneNumber });
+    }
     return (
         <div>
             <div className="user-form">
                 <label>FullName</label>
-                <input type="text" />
+                <Input
+                    value={fullName}
+                    onChange={((event) => { setFullName(event.target.value) })}
+                />
                 <label>Email</label>
-                <input type="text" />
+                <Input
+                    value={email}
+                    onChange={((event) => { setEmail(event.target.value) })}
+                />
                 <label>Password</label>
-                <Input.Password placeholder="input password" />
+                <Input.Password placeholder="Input password"
+                    value={passWord}
+                    onChange={((event) => { setpassWord(event.target.value) })}
+                />
                 <label>Phone Number</label>
-                <input type="text" />
+                <Input
+                    value={phoneNumber}
+                    onChange={((event) => { setphoneNumber(event.target.value) })}
+                />
                 <div>
 
-                    <Button type="primary" style={{ width: "150px" }}>Create User</Button>
+                    <Button type="primary" style={{ width: "150px" }}
+                        onClick={handleClickBtn}
+                    >Create User</Button>
                 </div>
             </div>
         </div >
